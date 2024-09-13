eaf4e3fe18c05183671d7be4fdcc1d91b55042ea
simonqq21 <ques86225@gmail.com>
2024-09-14 01:46:26 +0800

    basic wifi webpage functionality working

--- data/wifi.js
+++ data/wifi.js
@@ -1,108 +1,8 @@
-function requestWifis(ws) {
-    msg = {cmd: "request",
-        type: "wifis",
-    };
-    ws.send(JSON.stringify(msg));
-}
-
-function requestDateTime(ws) {
-    msg = {cmd: "request",
-        type: "datetime",
-    };
-    ws.send(JSON.stringify(msg));
-}
-
-function sendConnection(ws) {
-    msg = {};
-    msg["cmd"] = "save";
-    msg["type"] = "connection";
-    msg["payload"] = {};
-    payload = msg["payload"];
-    payload["ssid"] = connectSSID;
-    payload["pass"] = 
-    console.log(JSON.stringify(msg));
-}
-
-function updateDisplayTime(payload) {
-    document.getElementById("systemTime").textContent = payload["datetime"];
-}
-
-function updateWiFis(payload) {
-    console.log(JSON.stringify(payload));
-    let wifilisttbody = document.getElementById("wifilisttable").getElementsByTagName("tbody")[0];
-    let wifilistrows = wifilisttbody.getElementsByTagName("tr");
-    
-    while (wifilistrows.length) {
-        wifilisttbody.removeChild(wifilistrows[0]);
-    }
-    let wifiIndex = 0;
-    for (wifi of payload["wifis"]) {
-        console.log(wifi["ssid"]);
-        let wifitablerow = document.createElement("tr");
-        wifitablerow.classList.add("wifihotspot");
-        wifitablerow.setAttribute("wifiindex", wifiIndex);
-        wifiIndex++;
-
-        let wifiname = document.createElement("td");
-        wifiname.classList.add("wifiname");
-        wifiname.textContent = wifi["ssid"];
-        wifitablerow.appendChild(wifiname);
-
-        let wifisecurity = document.createElement("td");
-        wifisecurity.classList.add("wifisecurity");
-        wifisecurity.textContent = wifi["security"];
-        wifitablerow.appendChild(wifisecurity);
-
-        let wifirssi = document.createElement("td");
-        wifirssi.textContent = `${wifi["rssi"]} dBm`;
-        wifitablerow.appendChild(wifirssi);
-
-        let wificonnectbtntd = document.createElement("td");
-        let wificonnectbtn = document.createElement("button");
-        wificonnectbtn.textContent = "Connect";
-        wificonnectbtn.addEventListener("click", function() {
-            let wifilistrow = this.parentElement.parentElement;
-            connectSSID = wifilistrow.getElementsByClassName("wifiname")[0].textContent;
-            connectSecurity = wifilistrow.getElementsByClassName("wifisecurity")[0].textContent;
-            // alert(connectSecurity);
-            setPage("connect");
-        });
-        wificonnectbtn.id = "";
-        wificonnectbtntd.appendChild(wificonnectbtn);
-        wifitablerow.appendChild(wificonnectbtntd);
-        wifilisttbody.appendChild(wifitablerow);
-    }
-    // console.log(`l${wifilistrows.length}`);
-    // for (row of wifilistrows) {
-    //     console.log(row.getAttribute("wifiindex"));
-    // }
-}
-
-function setPage(pagename) {
-    let listwifipage = document.getElementById("listwifipage");
-    let wificonnectpage = document.getElementById("wificonnectpage");
-
-    if (pagename == "listwifi") {
-        listwifipage.style.display = "block";
-        wificonnectpage.style.display = "none";
-    }
-    else if (pagename == "connect") {
-        listwifipage.style.display = "none";
-        wificonnectpage.style.display = "block";
-        console.log(connectSecurity);
-        if (connectSecurity == "open") {
-            document.getElementById("passwordinputdiv").style.display = "none";
-        }
-        else {
-            document.getElementById("passwordinputdiv").style.display = "block";
-        }
-    }
-}
-
 document.addEventListener("DOMContentLoaded", function() {
     let port = 5555;
     // let hostname = window.location.hostname;
-    let hostname = "192.168.5.70";
+    let hostname = "192.168.5.71";
+    // let hostname = "192.168.4.1";
     let url = `ws://${hostname}:${port}/ws`;
     console.log(`hostname=${hostname}`);
     let ws = new WebSocket(url);
@@ -112,6 +12,115 @@ document.addEventListener("DOMContentLoaded", function() {
     let connectPass;
     let connectPort;
     let connectIPIndex;
+
+
+
+    function requestWifis(ws) {
+        msg = {cmd: "request",
+            type: "wifis",
+        };
+        ws.send(JSON.stringify(msg));
+    }
+    
+    function requestDateTime(ws) {
+        msg = {cmd: "request",
+            type: "datetime",
+        };
+        ws.send(JSON.stringify(msg));
+    }
+    
+    function sendConnection(ws) {
+        msg = {};
+        msg["cmd"] = "save";
+        msg["type"] = "connection";
+        msg["payload"] = {};
+        payload = msg["payload"];
+        payload["ssid"] = connectSSID;
+        payload["pass"] = connectPass;
+        payload["ipIndex"] = connectIPIndex;
+        payload["port"] = connectPort;
+        console.log(JSON.stringify(msg));
+        ws.send(JSON.stringify(msg));
+    }
+    
+    function updateDisplayTime(payload) {
+        document.getElementById("systemTime").textContent = payload["datetime"];
+    }
+    
+    function updateWiFis(payload) {
+        console.log(JSON.stringify(payload));
+        let wifilisttbody = document.getElementById("wifilisttable").getElementsByTagName("tbody")[0];
+        let wifilistrows = wifilisttbody.getElementsByTagName("tr");
+        
+        while (wifilistrows.length) {
+            wifilisttbody.removeChild(wifilistrows[0]);
+        }
+        let wifiIndex = 0;
+        for (wifi of payload["wifis"]) {
+            console.log(wifi["ssid"]);
+            let wifitablerow = document.createElement("tr");
+            wifitablerow.classList.add("wifihotspot");
+            wifitablerow.setAttribute("wifiindex", wifiIndex);
+            wifiIndex++;
+    
+            let wifiname = document.createElement("td");
+            wifiname.classList.add("wifiname");
+            wifiname.textContent = wifi["ssid"];
+            wifitablerow.appendChild(wifiname);
+    
+            let wifisecurity = document.createElement("td");
+            wifisecurity.classList.add("wifisecurity");
+            wifisecurity.textContent = wifi["security"];
+            wifitablerow.appendChild(wifisecurity);
+    
+            let wifirssi = document.createElement("td");
+            wifirssi.textContent = `${wifi["rssi"]} dBm`;
+            wifitablerow.appendChild(wifirssi);
+    
+            let wificonnectbtntd = document.createElement("td");
+            let wificonnectbtn = document.createElement("button");
+            wificonnectbtn.textContent = "Connect";
+            wificonnectbtn.addEventListener("click", function() {
+                let wifilistrow = this.parentElement.parentElement;
+                connectSSID = wifilistrow.getElementsByClassName("wifiname")[0].textContent;
+                connectSecurity = wifilistrow.getElementsByClassName("wifisecurity")[0].textContent;
+                // alert(connectSecurity);
+                setPage("connect");
+            });
+            wificonnectbtn.id = "";
+            wificonnectbtntd.appendChild(wificonnectbtn);
+            wifitablerow.appendChild(wificonnectbtntd);
+            wifilisttbody.appendChild(wifitablerow);
+        }
+        // console.log(`l${wifilistrows.length}`);
+        // for (row of wifilistrows) {
+        //     console.log(row.getAttribute("wifiindex"));
+        // }
+    }
+    
+    function setPage(pagename) {
+        let listwifipage = document.getElementById("listwifipage");
+        let wificonnectpage = document.getElementById("wificonnectpage");
+    
+        if (pagename == "listwifi") {
+            listwifipage.style.display = "block";
+            wificonnectpage.style.display = "none";
+        }
+        else if (pagename == "connect") {
+            listwifipage.style.display = "none";
+            wificonnectpage.style.display = "block";
+            console.log(connectSecurity);
+            if (connectSecurity == "open") {
+                document.getElementById("passwordinputdiv").style.display = "none";
+            }
+            else {
+                document.getElementById("passwordinputdiv").style.display = "block";
+            }
+        }
+    }
+
+
+
 
     ws.addEventListener("open", (event) => {
         requestWifis(ws);
@@ -139,11 +148,28 @@ document.addEventListener("DOMContentLoaded", function() {
         console.log("ws closed");
     });
 
+    let connectPassInput = document.getElementById("passwordinputdiv").getElementsByTagName("input")[0];
+    let connectPortInput = document.getElementById("portinputdiv").getElementsByTagName("input")[0];
+    let connectIPIndexInput = document.getElementById("ipindexdiv").getElementsByTagName("input")[0];
+
+    connectPassInput.addEventListener("change", function() {
+        connectPass = connectPassInput.value;
+        // alert(connectPass);
+    });
+
+    connectPortInput.addEventListener("change", function() {
+        connectPort = connectPortInput.value;
+        // alert(connectPort);
+    });
+
+    connectIPIndexInput.addEventListener("change", function() {
+        connectIPIndex = connectIPIndexInput.value;
+        // alert(connectIPIndex);
+    });
+
     document.getElementById("wificonnectpagebackbtn").addEventListener("click", function() {
         setPage("listwifi");
     });
-
-    document.getElementById()
     
     document.getElementById("wificonnectpageconnectbtn").addEventListener("click", function() {
         console.log("Connect!");
